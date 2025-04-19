8ce685e3985ac5f8b3334f09b85c00c51bc170db
Dipendra Tamang <dipendra.tamang9@gmail.com>
2025-04-19 09:59:42 +0545

    integrate

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1>Dipendra Tamang</h1>
-      </header>
-    </div>
-  );
-}
+    const [message, setMessage] = useState('');
 
+    useEffect(() => {
+        axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/hello`)
+            .then(res => setMessage(res.data));
+    }, []);
+
+    return <h1>{message || "Loading..."}</h1>;
+}
 export default App;
