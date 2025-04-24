a224216814fd15aa8b050ee105d974a1c9637031
Dipendra Tamang <dipendra.tamang9@gmail.com>
2025-04-24 18:46:04 +0545

    changes on navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -12,8 +12,8 @@ export default function Navbar() {
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
                         <li><a href="/">Homepage</a></li>
-                        <li><a>Portfolio</a></li>
-                        <li><a>About</a></li>
+                        <li><a href="/">Portfolio</a></li>
+                        <li><a  href="/">About</a></li>
                     </ul>
                 </div>
             </div>
